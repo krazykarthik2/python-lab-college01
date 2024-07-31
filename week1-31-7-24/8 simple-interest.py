d46eb2e61481f557b73073255f4ba69c77aed494
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 15:48:14 +0530

    Create 8 simple-interest.py

---
+++ week1-31-7-24/8 simple-interest.py
@@ -0,0 +1,6 @@
+#python program to calculate simple interest
+p = float(input("Enter the principal amount: "))
+r = float(input("Enter the rate of interest: "))
+t = float(input("Enter the time period: "))
+si = (p * r * t) / 100
+print("The simple interest is", si)
