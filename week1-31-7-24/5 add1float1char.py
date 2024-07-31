7c212c313f4e24cd94871a35519492813da5f32b
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 16:01:12 +0530

    Create 5 add1float1char.py

---
+++ week1-31-7-24/5 add1float1char.py
@@ -0,0 +1,7 @@
+# python  program to add 1 floating point number to 1 char
+
+a=float(input("enter the number"))
+b=(input("enter the number"))
+c=str(a)+b
+print("addition of an float and a character is :",end="")
+print(c)
