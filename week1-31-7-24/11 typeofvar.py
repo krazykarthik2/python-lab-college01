16ad0b91cebb582bbf58f95651f85db71559a9a5
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 15:51:18 +0530

    Create 11 typeofvar.py

---
+++ week1-31-7-24/11 typeofvar.py
@@ -0,0 +1,13 @@
+# python program to print type of variable
+a= 10
+print(type(a))
+b= 10.5
+print(type(b))
+c= "hello"
+print(type(c))
+d= 10+5j
+print(type(d))
+e= True
+print(type(e))
+f= None
+print(type(f))
