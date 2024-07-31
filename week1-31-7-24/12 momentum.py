2ac77e102e8574f7aed06c39cdb1a5aa1adb3b5e
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 15:52:14 +0530

    Create 12 momentum.py

---
+++ week1-31-7-24/12 momentum.py
@@ -0,0 +1,6 @@
+# python program to calculate momentum
+# formula for momentum is p = m * v
+m = float(input("Enter the mass: "))
+v = float(input("Enter the velocity: "))
+p = m * v
+print("The momentum is: ", p)
