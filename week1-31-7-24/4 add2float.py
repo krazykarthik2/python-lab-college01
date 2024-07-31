30e4366cb1bc1ba7e6bfbcd8e17f43bbbf4ec2b3
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 15:56:45 +0530

    Create 4 add2float.py

---
+++ week1-31-7-24/4 add2float.py
@@ -0,0 +1,5 @@
+#python program to add 2 float numbers
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+print("addition of two float numbers is :",end="")
+print(x+y)
