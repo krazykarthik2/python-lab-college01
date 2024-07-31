65ec14fe0e2f647960d4bdf795e827c5f8a3b44e
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 15:43:37 +0530

    Create 1int1float.py

---
+++ week1-31-7-24/1int1float.py
@@ -0,0 +1,6 @@
+# python program to add 1 integer and 1 float
+x = int(input("enter first value:"))
+y = float(input("enter second value:"))
+z = x + y
+print("addition of two values is:")
+print(z)
