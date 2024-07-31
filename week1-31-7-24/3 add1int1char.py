86e494820142dcd5272ae5984062bf9e388e047f
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-31 15:58:56 +0530

    Create 3 add1int1char.py

---
+++ week1-31-7-24/3 add1int1char.py
@@ -0,0 +1,5 @@
+# python program to add 1 integer and 1 character
+x = int(input("Enter an integer: "))
+y = input("Enter a character: ")
+print("addition of an integer and a character is :",end="")
+print(str(x)+y)
